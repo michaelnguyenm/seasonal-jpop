4a89b06302fb196a22cc3d9235f8c84ab60bfe1d
Michael Nguyen <michaelnguyenm@gmail.com>
2016-12-24 18:43:49 -0600

    Implementing html5 features, #2

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+var routerApp = angular.module('routerApp', ['ui.router']);
+
+routerApp.config(function ($locationProvider) {
+    $locationProvider.html5Mode(true);
+});
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'home.html'
+        });
+
+});
