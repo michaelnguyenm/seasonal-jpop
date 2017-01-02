bdeaadd0ff4ec6f670337440061b3ca634172911
Michael Nguyen <michaelnguyenm@gmail.com>
2017-01-02 03:28:25 -0600

    Moved angular-mocks

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules\angular-mocks\angular-mocks.js',
-      'test/**/*.test.js'
+      'test/**/*.test.js',
+      'node_modules\angular-mocks\angular-mocks.js'
     ],
 
 
