40a9e0b716f5c4dc4027a673648244d4bb114c55
Michael Nguyen <michaelnguyenm@gmail.com>
2017-01-02 05:15:42 -0600

    Modified order again

--- karma.conf.js
+++ karma.conf.js
@@ -17,11 +17,12 @@ module.exports = function(config) {
     files: [
       'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js',
       'https://code.angularjs.org/1.6.0/angular.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.3.2/angular-ui-router.min.js',
       'node_modules/angular-mocks/angular-mocks.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.3.2/angular-ui-router.min.js',
+
       'client/app/app.js',
-      'client/app/about/about.js',
       'client/app/home/home.js',
+      'client/app/about/about.js',
       'client/app/**/*.html',
       'test/client/app/app.test.js',
       'test/client/app/about/about.test.js',
